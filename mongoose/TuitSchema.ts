f06a8508f131c5cc2fc038fcb38932fdf47c4c34
mitchneides <mitch.neides@gmail.com>
2022-09-28 13:22:29 -0400

    changes postedBy to reference ID rather than user object

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 const TuitSchema = new mongoose.Schema({
     tuit: {type: String, required: true},
     postedOn: {type: Date, default: Date.now},
-    postedBy: {type: User, required: true}
-}), {collection: 'tuits'});
+    postedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}
+}, {collection: 'tuits'});
 export default TuitSchema;
+// .populate(postedBy)
+// see castings-dao
