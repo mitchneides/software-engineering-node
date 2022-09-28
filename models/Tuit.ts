f3276af9ab12119352740a2674a600e05d478b6e
mitchneides <mitch.neides@gmail.com>
2022-09-28 13:21:44 -0400

    adds constructor for tuit model

--- models/Tuit.ts
+++ models/Tuit.ts
@@ -4,4 +4,10 @@ export default class Tuit {
    private tuit: string = '';
    private postedOn: Date = new Date();
    private postedBy: User | null = null;
+
+   constructor(tuit: string, postedOn: Date, postedBy: User) {
+       this.tuit = tuit;
+       this.postedOn = postedOn;
+       this.postedBy = postedBy;
+   }
 }
