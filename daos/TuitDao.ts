e35d38e5db56716f0b5cf85a14aa441a89039342
mitchneides <mitch.neides@gmail.com>
2022-10-04 10:57:18 -0400

    writes findTuidById functionality

--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -12,7 +12,6 @@ export default class TuitDao implements TuitDaoI {
    async findTuitById(tid: string): Promise<Tuit> {
        const tuitObj = await TuitModel.findById(tid);
        return new Tuit(tuitObj.tuit, tuitObj.postedOn, tuitObj.postedBy);
-       // postedBy^ is now referencing an ID so how to connect this to the user now???
    }
 
 
