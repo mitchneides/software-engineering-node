ffcfe77ae0260a64d2d7f6c24eb23f71aff8fe85
mitchneides <mitch.neides@gmail.com>
2022-10-22 16:56:17 -0400

    activates userMessagesUser functionality

---
+++ interfaces/MessageDaoI.ts
@@ -0,0 +1,10 @@
+import Message from "../models/Message";
+
+export default interface FollowDaoI {
+    userMessagesUser (message: Message): Promise<Message>;
+//     findAllSentMessages (uid: string): Promise<Message[]>;
+//     findAllReceivedMessages (uid: string): Promise<Message[]>;
+//     deleteMessage (mid: string): Promise<any>;
+//     findAllMessagesTo (from_uid: string, to_uid: string): Promise<Message[]>;
+//     findAllMessagesFrom (from_uid: string, to_uid: string): Promise<Message[]>;
+}
