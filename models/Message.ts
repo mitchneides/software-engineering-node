f60307c1d465cd57aa8ec0f6519e088b19a77444
mitchneides <mitch.neides@gmail.com>
2022-10-22 16:54:33 -0400

    fixes bug in message model

--- models/Message.ts
+++ models/Message.ts
@@ -1,8 +1,15 @@
-import User from "User";
+import User from "./User";
 
 export default class Message {
-    message: String,
-    to: User,
-    from: User,
-    sentOn: Date
+    private message_text: string = '';
+    private to: User | null = null;
+    private from: User | null = null;
+    private sentOn: Date = new Date();
+
+    constructor(message_text: string, to: User, from: User, sentOn: Date) {
+        this.message_text = message_text;
+        this.to = to;
+        this.from = from;
+        this.sentOn = sentOn;
+    }
 }
