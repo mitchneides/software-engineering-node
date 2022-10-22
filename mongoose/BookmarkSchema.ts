2ce35afb42857baab9fef115a3a08a6527ea2bc2
mitchneides <mitch.neides@gmail.com>
2022-10-22 11:14:14 -0400

    specifies db collection name

--- mongoose/BookmarkSchema.ts
+++ mongoose/BookmarkSchema.ts
@@ -3,7 +3,7 @@ import Tuit from "../models/Tuit";
 import User from "../models/User";
 
 const BookmarkSchema = new mongoose.Schema({
-    tuit: {type: mongoose.Schema.Types.ObjectID, ref: 'TuitModel'},
-    bookmarkedBy: {type: mongoose.Schema.Types.ObjectID, ref: 'UserModel'}
-});
+    tuit: {type: mongoose.Schema.Types.ObjectId, ref: 'TuitModel'},
+    bookmarkedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}
+}, {collection: 'bookmarks'});
 export default BookmarkSchema;
