c112061ef59c401a0d653fcfc834dbfea85781a8
mitchneides <mitch.neides@gmail.com>
2022-10-22 11:46:07 -0400

    writes findAllBookmarksFromAuthor query

--- daos/BookmarkDao.ts
+++ daos/BookmarkDao.ts
@@ -3,11 +3,12 @@ import BookmarkModel from "../mongoose/BookmarkModel";
 import Bookmark from "../models/Bookmark";
 import User from "../models/User";
 import UserModel from "../mongoose/UserModel";
+import Tuit from "../models/Tuit";
 import TuitModel from "../mongoose/TuitModel";
 
 export default class BookmarkDao implements BookmarkDaoI {
     private static bookmarkDao: BookmarkDao | null = null;
-    public status getInstance = () : BookmarkDao => {
+    public static getInstance = () : BookmarkDao => {
         if (BookmarkDao.bookmarkDao == null) {
             BookmarkDao.bookmarkDao = new BookmarkDao();
         }
@@ -24,30 +25,27 @@ export default class BookmarkDao implements BookmarkDaoI {
         BookmarkModel.find({bookmarkedBy: uid})
                      .populate("tuit")
                      .exec();
-
-
-
-
-
-// come back to here
     findAllBookmarksFromAuthor = async (uid: string, aid: string): Promise<Tuit[]> => {
-        var allBookmarks = await BookmarkModel
-                                    .find({bookmarkedBy: uid})
+        var allBookmarkedTuits = await BookmarkModel
+                                    .where("bookmarkedBy")
+                                    .equals(uid)
                                     .populate("tuit")
-                                    .exec()
         var result = new Array();
-        allBookmarks.forEach(function (value) {
-            var t = await TuitModel.find({_id: value.tuit})
-                                   .populate('tuit')
-                                   .exec()
-
+        allBookmarkedTuits.forEach(function (value) {
+            if (value.tuit.postedBy == aid) {
+                result.push(value);
+            }
         });
+        return result;
     }
 
 
-// and here
+// TODO:
     findAllBookmarksAfterDate = async (uid: string, date: string): Promise<Tuit[]> => {
-
+        // dummy return for testing - DELETE
+        return await BookmarkModel.find({bookmarkedBy: uid})
+                     .populate("tuit")
+                     .exec();
     }
 
 
